9f49aad7c05e7c93ed6e901fc39eaf34908b3dd5
Noemi Castaneda <noemicastaneda7@gmail.com>
2026-05-08 16:28:17 -0700

    Update pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Wife":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 5 5 5 5 . . . . . . . . 
+. . . . 5 f f 5 5 . . . . . . . 
+. . . 5 5 5 5 5 5 5 . . . . . . 
+. . . 5 2 2 2 2 5 . . . . . . . 
+. . . . 5 5 5 5 5 . . f f . . . 
+. . . . . 5 5 a a . f f . . . . 
+. . . . . a a a a f f . . . . . 
+. f f f f a a a a a a a . . . . 
+. . . . a a a a a a a a a . . . 
+. . . a a a a . . f . . . . . . 
+. . . . . f . . . f . . . . . . 
+. . . . . f . . . f . . . . . . 
+. . . . . f . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
